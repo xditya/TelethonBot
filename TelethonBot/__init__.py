cdd555889472d93171b8d677a81ed24cbde76f9b
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-05 18:12:02 +0530

    Update __init__.py

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -14,4 +14,4 @@ APP_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
-BotzHub = TelegramClient('BotzHub', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+BotzHub = TelegramClient('BotzHub', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
